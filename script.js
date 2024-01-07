0430e570ebbdd85baff666681e8764fefe42fda5
Aryan568 <aryanpatidar730@gmail.com>
2024-01-07 18:51:36 +0530

    api implementation

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const apiKey= "9559e5ad07f244f3815110547240701"
-const apiUrl= "https://api.weatherapi.com/v1/current.json?key=9559e5ad07f244f3815110547240701&q=Indore"
+const apiUrl= "https://api.weatherapi.com/v1/current.json?key=9559e5ad07f244f3815110547240701&q="
 
-async function chkWeather(){
-    const response= await fetch(apiUrl + '&appid=${apiKey}')
-    
+const searchBox= document.querySelector(".search input")
+const searchBtn= document.querySelector(".search button")
+
+async function chkWeather(city){
+    const response= await fetch(apiUrl + city + '&appid=${apiKey}')
+    var data = await response.json()
+    console.log(data)
+    document.querySelector(".city").innerHTML= data.location.name
+    document.querySelector(".temp").innerHTML= data.current.temp_c + "°c"
+    document.querySelector(".humidity").innerHTML= data.current.humidity + "%"
+    document.querySelector(".wind").innerHTML= data.current.wind_kph + "km/h"
+
+
 }
+
+searchBtn.addEventListener("click", ()=>{
+    chkWeather(searchBox.value)
+})
